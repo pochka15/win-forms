9770199422dc2c235eab2e6b82decdf0596ec2f9
Artsiom Kashkou <a.kashkou@stud.elka.pw.edu.pl>
2021-12-22 10:47:53 +0100

    Prohibit having 0 MDI children

--- lab/MainForm.cs
+++ lab/MainForm.cs
@@ -7,6 +7,9 @@ namespace lab {
 public partial class MainForm : Form {
     private readonly TracksState _tracksState;
 
+    // counter is used to enable and disable close buttons of forms
+    private int _childCounter;
+
     public MainForm() {
         InitializeComponent();
         _tracksState = new TracksState(TracksState.BuildDummyTracks(5));
@@ -20,16 +23,30 @@ public partial class MainForm : Form {
             control.BackColor = Color.AliceBlue;
             break;
         }
+
+        AddMusicForm();
     }
 
     private void AddMusicForm() {
-        var counter = 1 + MdiChildren.Length;
-        var form = new MusicForm(_tracksState, counter) {MdiParent = this};
-        form.Activated += (o, args) => UpdateActiveForm(form);
-        form.Activated += (o, args) => MergeMenus(form);
+        _childCounter += 1;
+        var form = new MusicForm(_tracksState, _childCounter) {MdiParent = this};
+        form.Activated += (o, args) => HandleFormActivated(form);
         form.Deactivate += (o, args) => UnmergeMenus(form);
+        form.Closed += (o, args) => HandleChildClosed();
         form.Show();
+        UpdateChildControlBoxes();
     }
+
+    private void HandleChildClosed() {
+        _childCounter -= 1;
+        UpdateChildControlBoxes();
+    }
+
+    private void UpdateChildControlBoxes() {
+        var isEnabled = _childCounter != 1;
+        foreach (var child in MdiChildren) child.ControlBox = isEnabled;
+    }
+
 
     private void UnmergeMenus(MusicForm target) {
         ToolStripManager.RevertMerge(upperMenuStrip, target.UpperMenuStrip);
@@ -39,9 +56,11 @@ public partial class MainForm : Form {
         ToolStripManager.Merge(source.UpperMenuStrip, upperMenuStrip);
     }
 
-    private void UpdateActiveForm(MusicForm form) {
+    private void HandleFormActivated(MusicForm form) {
         // ReSharper disable once LocalizableElement
         bottomStatusBar.Text = $"Form {form.Id}";
+        MergeMenus(form);
+        UpdateChildControlBoxes();
     }
 
     private void addTrackToolStripMenuItem_Click(object sender, EventArgs e) {
